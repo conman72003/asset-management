6439ae798c6bebe5cc725b72e77489abfc5b2fe8
Conner O'Brien <connerobrien07@gmail.com>
2025-09-29 00:05:28 -0400

    refinements to filter

--- JS/components/filter.js
+++ JS/components/filter.js
@@ -20,7 +20,26 @@ export function initFilters() {
             if (!isOpen) {
                 wrapper.classList.add('is-open');
             }
+            
         });
+
+        const searchInput = wrapper.querySelector('.filters__dropdown-search');
+        if (searchInput) {
+            const options = wrapper.querySelectorAll('.filters__dropdown-options .custom-checkbox');
+
+            searchInput.addEventListener('keyup', () => {
+                const searchTerm = searchInput.value.toLowerCase();
+
+                options.forEach(option => {
+                    const labelText = option.textContent.toLowerCase();
+                    if (labelText.includes(searchTerm)) {
+                        option.style.display = 'flex';
+                    } else {
+                        option.style.display = 'none';
+                    }
+                });
+            });
+        }
     });
 
     document.addEventListener('click', (event) => {
@@ -29,4 +48,3 @@ export function initFilters() {
         }
     });
 }
-
