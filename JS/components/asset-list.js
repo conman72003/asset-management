9b68e19228fb7a353fdee11c5b8868e2e63abbef
Conner O'Brien <connerobrien07@gmail.com>
2025-10-05 23:54:18 -0400

    moved action button

--- JS/components/asset-list.js
+++ JS/components/asset-list.js
@@ -1,12 +1,32 @@
 export function initAssetList() {
+    const assetList = document.querySelector('.asset-list');
     const selectAllCheckbox = document.getElementById('select-all-checkbox');
     const itemCheckboxes = document.querySelectorAll('.asset-list__row .custom-checkbox input[type="checkbox"]');
+
+    
+    const allCheckboxes = [selectAllCheckbox, ...itemCheckboxes];
+
+    function updateSelectionState() {
+        const selectedCount = [...itemCheckboxes].filter(cb => cb.checked).length;
+
+        if (selectedCount > 0) {
+            assetList.classList.add('selection-active');
+            selectionCountSpan.textContent = `${selectedCount} Selected`;
+        } else {
+            assetList.classList.remove('selection-active');
+        }
+    }
 
     if (selectAllCheckbox) {
         selectAllCheckbox.addEventListener('change', () => {
             itemCheckboxes.forEach(checkbox => {
                 checkbox.checked = selectAllCheckbox.checked;
             });
+            updateSelectionState(); 
         });
     }
+
+    itemCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', updateSelectionState);
+    });
 }
